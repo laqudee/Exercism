6af8bff1e65e8262597256c3076bcd66f6d58c90
Laqudee Des <24524492+laqudee@users.noreply.github.com>
2023-12-27 16:09:05 +0800

    update c code of isogram

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,1 +1,30 @@
 #include "isogram.h"
+
+bool is_isogram(const char phrase[]) {
+    if (phrase == NULL) {
+        return false;
+    }
+    int letter_array[26] = {0};
+    int i = 0;
+    char ch = phrase[0];
+    while (ch != '\0') {
+      printf("%c, ", ch);
+        if (isalpha(ch)) {
+            int array_index = tolower(ch) - 'a';
+            printf("%d, letter_array %d \n", array_index, letter_array[array_index]);
+            if (letter_array[array_index]) {
+                return false;
+            }
+            ++letter_array[array_index];
+        }
+        ch = phrase[++i];
+    }
+    return true;
+}
+
+int main(void) {
+  int result = is_isogram("cffrefg");
+  printf("%d \n", result);
+
+  return 0;
+}
--- c/isogram/isogram.h
+++ c/isogram/isogram.h
@@ -1,7 +1,10 @@
 #ifndef ISOGRAM_H
 #define ISOGRAM_H
 
+#include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
 bool is_isogram(const char phrase[]);
 
